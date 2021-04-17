4fde10bb4e56804fd87bd369a09a1561b9404655
Iryna Haiduk <$ git config --global user.name Iryna>
2021-04-17 23:01:07 +0300

    Fixed the behavior of forms when submitting data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,16 @@
 
 const mobileMenu = document.querySelector('.js-header__menu');
 const menuToggler = document.querySelector('.js-menu-toggler');
+const formsList = document.querySelectorAll('form');
 
 menuToggler.addEventListener('click', function() {
   menuToggler.classList.toggle('menu-toggler--active');
   mobileMenu.classList.toggle('header__menu--active');
+});
+
+formsList.forEach(form => {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form.reset();
+  })
 });
